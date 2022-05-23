cef446336da73d837c4b53d575cf192589100c20
IvanDP12 <ikunev26@gmail.com>
2022-05-23 14:26:15 +0300

    main layout done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Filters from './components/Filters/Filters'
+import CharacterCard from './components/CharacterCard/CharacterCard'
 
 function App() {
   return (
-    <div className='container'>
+    <div className='App'>
       <h1 className='text-center'>The Rick and Morty API</h1>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-2'><Filters/></div>
+          <div className='col-10'>
+            <div className='row card-section'>
+            <CharacterCard/>
+            <CharacterCard/>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
