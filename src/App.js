56b06683e19671fae9e7e5c421c93e6b98582471
IvanDP12 <ikunev26@gmail.com>
2022-05-23 12:30:26 +0300

    all the folders and files created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className=''>
+    <div className='container'>
+      <h1 className='text-center'>The Rick and Morty API</h1>
     </div>
   )
 }
